00136ab304fe363b99ae3fb352f490804b5f5f22
vfariaAiko <vfaria@aiko.digital>
2025-04-10 11:45:30 -0300

    Primeiro commit fix: syntax error

--- Teste/src/Teste.java
+++ Teste/src/Teste.java
@@ -2,7 +2,7 @@ public class Teste {
   public static void main(String[] args) {
     int num1 = 3, num2 = 5, soma;
     soma = num1 + num2;
-    system.out.println(soma);
+    System.out.println(soma);
   }
   
 }
