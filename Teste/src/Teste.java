1b68ae8ec8c025fe4de489094936e9af6e95b154
vfariaAiko <vfaria@aiko.digital>
2025-04-10 11:42:56 -0300

    Update Teste.java

--- Teste/src/Teste.java
+++ Teste/src/Teste.java
@@ -1,7 +1,8 @@
 public class Teste {
   public static void main(String[] args) {
-    int num1 = 3, num3 = 5, soma;
+    int num1 = 3, num2 = 5, soma;
     soma = num1 + num2;
+    system.out.println(soma);
   }
   
 }
